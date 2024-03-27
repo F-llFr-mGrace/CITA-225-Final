7a13ea3d96f35bfee07d279640915f5b83a07d4b
Grace Fowler <f.llfr.mgrace@gmail.com>
2024-03-27 12:46:40 -0400

    All commands added to help menu

--- FinalProject/FinalProject/FinalProject.py
+++ FinalProject/FinalProject/FinalProject.py
@@ -6,5 +6,11 @@ while isRun == True:
     userInput = str(input(""))
     if userInput == "help" or userInput == "Help":
         print("Command list")
+        print("add || Add a new product")
+        print("remove || Remove a product")
+        print("update || Update a specific product")
+        print("display || Display entire inventory")
+        print("help || this menu")
+        print("exit || close application")
     if userInput == "exit" or userInput == "Exit":
         isRun == False
