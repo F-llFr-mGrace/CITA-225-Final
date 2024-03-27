47ad37c33aa99ceb7b3e58cb89a8e2ac23eaf949
Grace Fowler <f.llfr.mgrace@gmail.com>
2024-03-27 13:11:08 -0400

    Created linked list of dictionaries

--- FinalProject/FinalProject/FinalProject.py
+++ FinalProject/FinalProject/FinalProject.py
@@ -1,3 +1,41 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class ProductLine:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data)
+            current = current.next
+
+# Create a product line linked list
+product_line = ProductLine()
+
+# Append products to the product line
+product_line.append({"name": "Product 1", "price": 10})
+product_line.append({"name": "Product 2", "price": 20})
+product_line.append({"name": "Product 3", "price": 30})
+
+# Display the product line
+product_line.display()
+
+#------------------
+
 isRun = True
 while isRun == True:
     userInput = None
