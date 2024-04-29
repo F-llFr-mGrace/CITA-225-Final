4b0716910f77e3c06cbefd3f6c0705581cef8a7e
Grace Fowler <f.llfr.mgrace@gmail.com>
2024-04-29 15:48:46 -0400

    Added cart linked list

--- FinalProject/FinalProject/FinalProject.py
+++ FinalProject/FinalProject/FinalProject.py
@@ -23,6 +23,27 @@ class ProductLine:
             print(current.data)
             current = current.next
 
+# Cart class added here
+class Cart:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def display(self):
+        current = self.head
+        while current:
+            print(current.data)
+            current = current.next
+
 # Create a product line linked list
 product_line = ProductLine()
 
@@ -35,30 +56,32 @@ product_line.append({"name": "Product 3", "price": 30})
 
 #------------------
 
-#Multi-purpose commands
+# Multi-purpose commands
 def AddItem():
+    itemName = str(input("Please enter item name: "))
+    itemPrice = str(input("Please enter item price: "))
     print("Adding item...")
-    
+
 def RemoveItem():
     print("Removing item...")
-    
-#Single-purpose commands
+
+# Single-purpose commands
 def UpdateItem():
     print("Updating item...")
-    
+
 def Undo():
     print("Removing most recent addition...")
-    
-#General Commands
+
+# General Commands
 def DisplayItems(type):
     if type == "c":
         print("Displaying cart...")
-        
+
     if type == "d":
         print("Displaying all products...")
         product_line.display()
-        
-#Exit executed within the loop
+
+# Exit executed within the loop
 
 runIMS = False
 runEcom = False
@@ -69,8 +92,8 @@ if userAuthenticate == "o":
     authCode = ""
     userCode = str(input("Enter password: "))
     if authCode == userCode:
-        runIMS = True 
-        
+        runIMS = True
+
 if userAuthenticate == "c":
     runEcom = True
 
@@ -79,32 +102,32 @@ if runIMS == True or runEcom == True:
     while isRun == True:
         userInput = None
         print("----------------------------------------")
-        
+
         if runIMS == True:
             print("Grace Corp IMS || V1.0")
-            
+
         if runEcom == True:
-            print("Grace Corp eCommerce || V1.0")    
-            
+            print("Grace Corp eCommerce || V1.0")
+
         print("Type 'help' for a full list of commands")
         print("")
 
         userInput = str(input(""))
-        userInput = userInput.lower() #Makes lowercase
-        
+        userInput = userInput.lower()  # Makes lowercase
+
         print("")
-    
+
         if userInput == "help" or userInput == "Help":
             print("Command list")
             print("")
-            
+
             if runIMS == True:
                 print("IMS specific")
                 print("add || Add a new product")
                 print("remove || Remove a product")
                 print("update || Update a specific product")
                 print("")
-                
+
             if runEcom == True:
                 print("Cart specific")
                 print("add || Add a product to your cart")
@@ -113,33 +136,33 @@ if runIMS == True or runEcom == True:
                 print("based on most recently added")
                 print("cart || Display entire inventory")
                 print("")
-                
+
             print("General commands")
             print("display || Display entire inventory")
             print("help || This menu")
             print("exit || Close application")
-        
-        #Multi-purpose commands
+
+        # Multi-purpose commands
         if userInput == "add":
             AddItem()
-    
+
         if userInput == "remove":
             RemoveItem()
-    
-        #Single-purpose commands
+
+        # Single-purpose commands
         if userInput == "update":
             UpdateItem()
-            
+
         if userInput == "undo":
             break
-        
+
         if userInput == "cart":
             DisplayItems("c")
-    
-        #General Commands
+
+        # General Commands
         if userInput == "display":
             DisplayItems("d")
-    
+
         if userInput == "exit":
             isRun == False
             break
